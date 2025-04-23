e0f51005840b09cd8ebf4ecf6b76210e16e3798b
julCalmeida <juliacalmeida333@gmail.com>
2025-04-23 18:57:34 -0300

    Update AULA_05.sql

--- AULA_05.sql
+++ AULA_05.sql
@@ -1,5 +1,5 @@
---Questão 01. Crie uma tabela pessoa com os campos ID, nome, sobrenome e idade. 
---Crie uma cláusula de checagem para o campo idade, impedindo que valores menores que 0 sejam adicionados.
+--Questão 01: Crie uma tabela pessoa com os campos ID, nome, sobrenome e idade, com um clausula de checagem no campo idade, limitando os valores ( > 0)
+
 CREATE TABLE Pessoa (
     ID int,
     Nome VARCHAR(50),
@@ -7,16 +7,17 @@ CREATE TABLE Pessoa (
     Idade INT check(Idade >= 0)
 );
 
---Questão 02. Altere a tabela pessoa e crie uma restrição utilizando a especificação unique ( A1, A2, …, Am) 
---para os campos ID, nome e sobrenome.
+--Questão 02: Altere a tabela pessoa e crie uma restrição utilizando a especificação unique para campos id, nome e sobrenome 
+
 ALTER TABLE Pessoa
 ADD CONSTRAINT Uni_pessoa UNIQUE (ID, Nome, Sobrenome);
 
---Altere a coluna idade da tabela pessoa e garanta que ela não receba valores nulos.
+--Questão 03: Altere a coluna idade da tabela pessoa e garanta que ela não receba valores nulos.
 ALTER TABLE Pessoa
 ALTER COLUMN Idade int not null;
---Crie uma tabela endereco com os campos ID e rua. Adicione o campo endereco na tabela pessoa 
---crie uma integridade referencial a partir deste campo com a tabela endereco.
+
+--Questão 04.1: Crie uma tabela endereco com os campos ID e rua. Adicione o campo endereco na tabela pessoa 
+--Questão 04.2: crie uma integridade referencial a partir deste campo com a tabela endereco.
 
 CREATE TABLE Endereco (
     ID int primary key,
